d6aa9680c94f6d80937d1e844638a240f081df3a
LudditeDeveloper19471 <78110259+Edgy13YearOld@users.noreply.github.com>
2022-03-02 11:53:51 -0500

    A little stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.smartdashboard.*;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
@@ -16,14 +17,12 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
  */
 public class Robot extends TimedRobot {
   private final PWMSparkMax m_leftMotor0 = new PWMSparkMax(0);
-  private final PWMSparkMax m_leftMotor1 = new PWMSparkMax(2);
-  private final MotorControllerGroup m_left = new MotorControllerGroup(m_leftMotor0, m_leftMotor1);
   private final PWMSparkMax m_rightMotor0 = new PWMSparkMax(1);
-  private final PWMSparkMax m_rightMotor1 = new PWMSparkMax(3);
-  private final MotorControllerGroup m_right = new MotorControllerGroup(m_rightMotor0, m_rightMotor1);
   
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_left, m_right);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor0, m_rightMotor0);
   private final Joystick m_stick = new Joystick(0);
+
+  private Timer elapsedTime = new Timer();
 
   @Override
   public void robotInit() {
@@ -34,10 +33,22 @@ public class Robot extends TimedRobot {
   }
 
   @Override
+  public void autonomousInit(){
+      elapsedTime.reset();
+      elapsedTime.start();
+      while(elapsedTime.get() < 1){
+        SmartDashboard.putString("DB/String 0", String.format("%.2f", elapsedTime.get()));
+        m_robotDrive.arcadeDrive(0.5, 0);
+      }
+      m_robotDrive.arcadeDrive(0, 0);
+  }
+
+  @Override
   public void teleopPeriodic() {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(-m_stick.getY(), m_stick.getX());
+    m_robotDrive.arcadeDrive(-m_stick.getY(), -m_stick.getX());
+    
   }
 }
