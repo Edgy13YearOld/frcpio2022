03d848e49333c2ea22e5786d75b4ae40674c6f95
BigBrainAhmed <94079747+BigBrainAhmed@users.noreply.github.com>
2022-03-21 18:06:15 -0400

    New Controller and CAN wiring.
    hot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,9 +5,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-//import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.*;
 //import edu.wpi.first.cameraserver.CameraServer;
@@ -19,27 +22,31 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Robot extends TimedRobot{
-  //Primary Controller
-  private final Joystick m_stick = new Joystick(0);
-  //Motor Controllers
-  private final PWMSparkMax m_leftMotor0 = new PWMSparkMax(0);
-  private final VictorSP m_leftMotor1 = new VictorSP(1);
-  private final MotorControllerGroup m_leftDriveMotors = new MotorControllerGroup(m_leftMotor0, m_leftMotor1);
-  private final PWMSparkMax m_rightDriveMotors = new PWMSparkMax(2);
-  private final PWMSparkMax m_intakeMotor = new PWMSparkMax(3);
-  private final PWMSparkMax m_shooterMotor = new PWMSparkMax(4);
-  private final VictorSP m_conveyerMotor = new VictorSP(5);
+  private final Joystick stick = new Joystick(0);
+  private final XboxController controller = new XboxController(1);
+  private final PWMSparkMax leftMotor0 = new PWMSparkMax(0);
+  private final VictorSP leftMotor1 = new VictorSP(1);
+  final JoystickButton button = new JoystickButton(controller, 6);
+  private final MotorControllerGroup leftDriveMotors = new MotorControllerGroup(leftMotor0, leftMotor1);
+  private final PWMSparkMax rightDriveMotors = new PWMSparkMax(2);
+  public static final int intakeSparkMaxID = 1;
+  private CANSparkMax intakeMotor;
+  //private final PWMSparkMax m_intakeMotor = new PWMSparkMax(3);
+  //private final PWMSparkMax m_shooterMotor = new PWMSparkMax(4);
+  //private final VictorSP m_conveyerMotor = new VictorSP(5);
   //Drivetrain
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDriveMotors, m_rightDriveMotors);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(leftDriveMotors, rightDriveMotors);
   //Pneumatics
   private final PneumaticsControlModule pCModule = new PneumaticsControlModule();//You wouldn't believe what this is
   DoubleSolenoid intakePneumatic = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 7, 6);
   //Intake Management
   private int intakeDown = 0;
   //Timekeeping
-  //private Timer elapsedTime = new Timer();
+  private Timer elapsedTime = new Timer();
   //Parameters for the camera
   //private final int IMG_WIDTH = 320;
   //private final int IMG_HEIGHT = 240;
@@ -54,9 +61,10 @@ public class Robot extends TimedRobot{
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightDriveMotors.setInverted(true);
-    m_conveyerMotor.setInverted(true);
-    m_shooterMotor.setInverted(true);
+    rightDriveMotors.setInverted(true);
+    intakeMotor = new CANSparkMax(intakeSparkMaxID, MotorType.kBrushless);
+    //m_conveyerMotor.setInverted(true);
+    //m_shooterMotor.setInverted(true);
     //pCModule.disableCompressor();
     //Camera Init
     /*
@@ -118,12 +126,29 @@ public class Robot extends TimedRobot{
 
   @Override
   public void teleopPeriodic() {
-    // Drive with arcade drive.
-    // That means that the Y axis drives forward
-    // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(-m_stick.getY(), -m_stick.getX());
+    m_robotDrive.arcadeDrive(-stick.getY(), -stick.getX());
+
+    
+    if(stick.getRawButton(1)){
+      intakeMotor.set(-0.25);
+      SmartDashboard.putString("DB/String 1", "poo");
+    } else {
+      intakeMotor.set(0);
+    }
+    
+
+    
+    if(controller.getRawButton(6)){
+      intakeMotor.set(-0.25);
+      SmartDashboard.putString("DB/String 1", "intake on");
+    } else {
+      intakeMotor.set(0);
+      SmartDashboard.putString("DB/String 1", "intake off");
+    }
+    
+    
     //Intake functions
-    if(m_stick.getRawButton(5)){//Button 5 rejects
+    /*if(m_stick.getRawButton(5)){//Button 5 rejects
       m_intakeMotor.set(0.4* intakeDown);
       m_conveyerMotor.set(0.4);
     }else if(m_stick.getRawButton(3)){//Button 3 intakes
@@ -133,6 +158,7 @@ public class Robot extends TimedRobot{
       m_intakeMotor.set(0);
       m_conveyerMotor.set(0);
     }
+    
     //Shooter function
     if(m_stick.getRawButton(1)){//Trigger
       m_shooterMotor.set(-m_stick.getThrottle()*0.5+0.5);
@@ -141,11 +167,12 @@ public class Robot extends TimedRobot{
     }
     SmartDashboard.putString("DB/String 0", String.format("SHOOTER THROTTLE %.2f", -m_stick.getThrottle()*0.5+0.5));
     //Solenoid Functions
-    if(m_stick.getRawButton(4)){
+    */
+    if(stick.getRawButton(4)){
       SmartDashboard.putString("DB/String 1", "INTAKE RETRACT");
       intakePneumatic.set(DoubleSolenoid.Value.kReverse);
       intakeDown = 0;
-    }else if(m_stick.getRawButton(6)){
+    }else if(stick.getRawButton(6)){
       SmartDashboard.putString("DB/String 1", "INTAKE EXTEND");
       intakePneumatic.set(DoubleSolenoid.Value.kForward);
       intakeDown = 1;
@@ -153,5 +180,6 @@ public class Robot extends TimedRobot{
       SmartDashboard.putString("DB/String 1", "INTAKE off");
       intakePneumatic.set(DoubleSolenoid.Value.kOff);
     }
+    
   }
 }
